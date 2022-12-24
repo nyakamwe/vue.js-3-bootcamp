ab6466d63d13bc03b121ff499fc8b6d306c27521
nyakamwe <nyakamweaimable@gmail.com>
2022-12-24 17:30:48 +0200

    Leaning vue 3 bacic - vue cli - vue files and templates - template refs - multiple components - scoped styles and global styles - props and slots(default, named) - emitting custom events - click event modifiers - using teleport

---
+++ vue-full-project/project/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <h3>{{ title }}</h3>
+  <input type="text" ref="name">
+  <div v-if="showButton">
+    <button v-on:click="handleClick">click me</button>
+  </div>
+  <button @click.shift.alt="toogleButton">show button</button>
+  <FormVue :minimum="minimum" @close="toogleButton">
+    <h2>Donate Form</h2>
+  </FormVue>
+</template>
+
+<script>
+import FormVue from './components/Form.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FormVue
+  },
+  data(){
+    return {
+      title: 'First Vue Projct',
+      minimum: 100,
+      showButton: false
+    }
+  },
+  methods:{
+    handleClick(){
+      this.$refs.name.focus()
+      this.$refs.name.classList.add('active')
+    },
+    toogleButton(){
+      this.showButton = !this.showButton
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-full-project/project/src/components/Form.vue
@@ -0,0 +1,52 @@
+<template>
+    <slot></slot>
+    <form action="">
+        <div class="row">
+            <div class="amount-card" @click="closeButton">
+                200
+            </div>
+            <div class="amount-card">
+                500
+            </div>
+            <div class="amount-card">
+                700
+            </div>
+        </div>
+        <div class="row">
+            <div class="amount-card">
+                {{ minimum }}
+            </div>
+        </div>
+        <label for="">Amount</label>
+        <input type="number" name="amount" >
+        <button>Pay</button>
+    </form>
+</template>
+
+<script>
+export default {
+    props: ['minimum'],
+    methods: {
+        closeButton(){
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style>
+    .row{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        text-align: center;
+    }
+    .amount-card{
+        background-color:blue;
+        color: #fff;
+        padding: 20px 40px;
+        border-radius: 4px;
+        margin: 20px;
+    }
+
+</style>
